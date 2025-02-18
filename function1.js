b9a6f66ed5a706c9b3b0b8b172da4950255bf66b
Balajireddyk4361 <balajireddyk4451@gmail.com>
2025-02-18 10:18:53 +0530

    learning javascript

---
+++ function1.js
@@ -0,0 +1,25 @@
+//function add(a,b){
+//let c=1;
+  //  return a+b+c
+//}
+//let r1=add(10,20)
+//console.log(r1)
+
+//let cdt=new Date().getFullYear();
+//console.log(cdt)
+
+let dt=new Date()
+console.log("current date:",dt.getDate())
+console.log("current Month:",dt.getMonth())
+console.log("current FullYear:",dt.getFullYear())
+
+//function cal_Age(birth_year){
+    //let current_year=new Date().getFullYear()
+  //  return current_year - birth_year;
+//}
+//let r_Age =cal_Age(1973)
+//let s_Age =cal_Age(2002)
+//let p_Age =cal_Age(2004)
+//console.log( r_Age)
+//console.log( s_Age)
+//console.log( p_Age)
